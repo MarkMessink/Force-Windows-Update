09762d295a6cfa7b5943adc121984e296906e161
Mark Messink <65164974+MarkMessink@users.noreply.github.com>
2020-11-02 14:38:12 +0100

    Update ForceWindowsBuildUpdate.ps1

--- ForceWindowsBuildUpdate.ps1
+++ ForceWindowsBuildUpdate.ps1
@@ -2,6 +2,10 @@
 .SYNOPSIS
     Force Windows Update Powershell script
 	Mark Messink 16-10-2020
+	
+	info: https://www.powershellgallery.com/packages/PSWindowsUpdate/
+	
+	Check buildnumbers: https://docs.microsoft.com/en-us/windows/release-information/
 
 .DESCRIPTION
 	This script updates Windows when buildnumber is to low
@@ -16,7 +20,6 @@
   
 .NOTES
   Executing this script may take a long time to finish
-  Check buildnumbers --> https://docs.microsoft.com/en-us/windows/release-information/
   1903 = 10.0.18362.0
   1909 = 10.0.18363.0
   2004 = 10.0.19041.0
@@ -24,11 +27,11 @@
   21H1 - 10.0.20231.0 (Insider preview DEV)
 
 .EXAMPLE
-  .\ForceWindowsUpdate.ps1
+  .\ForceWindowsBuildUpdate.ps1
 #>
 
 # Buildnumber to update to:
-$buildnumber = "10.0.19041.0"
+$buildnumber = "10.0.18363.0"
 
 # Create Default Intune Log folder (is not exist)
 $path = "C:\IntuneLogs"
@@ -46,16 +49,17 @@ Start-Transcript $logPath -Append -Force
 	Write-Output "----- Check version Windows"
 	$version = [system.environment]::OSversion.version
 	Write-Output "----- Windows version = $version"
-	if ($version -lt "$buildnumber") {    
-	Write-Output "----- Install NuGet Provider"
-	Install-PackageProvider -Name NuGet -Force
-	Write-Output "----- Install PSWindowsUpdate"
-	Install-Module -Name PSWindowsUpdate -Force
-	Write-Output "----- Install Windows Updates (no reboot)"
-	Get-WindowsUpdate -AcceptAll -Download -Install -IgnoreReboot | FT
-	Write-Output "----- Check new version Windows"
-	$version_new = [system.environment]::OSversion.version
-	Write-Output "----- New Windows version = $version_new"
+	if ($version -lt "$buildnumber") { 
+		Write-Output "----- Install NuGet Provider"
+		Install-PackageProvider -Name NuGet -MinimumVersion "2.8.5.201" -Force #-Verbose
+		Write-Output "----- Install Module PSWindowsUpdate"
+		Install-Module -Name PSWindowsUpdate -Force #-Verbose
+		Write-Output "----- Import module PSWindowsUpdate"
+		import-module PSWindowsUpdate #-Verbose
+		# Write-Output "----- List Windows Updates"
+		# Get-WindowsUpdate | FT KB, Size, Title
+		Write-Output "----- Accept, Download and Install Windows Updates (noReboot)"
+		Get-WindowsUpdate -AcceptAll -Download -Install -IgnoreReboot | FT Result, KB, Title
 	}
 	else {
 	Write-Output ""
@@ -63,6 +67,7 @@ Start-Transcript $logPath -Append -Force
 	}
 	
     Write-Output "-------------------------------------------------------------------"
+	
 
 #Stop Logging
 Stop-Transcript
